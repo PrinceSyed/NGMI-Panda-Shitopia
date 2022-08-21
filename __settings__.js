d0f2171ec900c1365d5e14331a6f987bd26b3ebe
Spike <syedibrahim1388@gmail.com>
2022-08-21 00:41:35 -0400

    Panda Toilet

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1490061.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "low-power"
+};
+SCRIPTS = [ 95945244, 95945240, 95945239, 95945217, 95945219, 95945291, 95945209, 95945218, 95945241, 95945295, 95945242, 95945243, 95945286, 95945310, 95945406, 95945424 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/95945214/1/basis.wasm.js', 'wasmUrl' : 'files/assets/95945211/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/95945216/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/95945210/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/95945220/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/95945222/1/ammo.js', 'preload' : true},
+];
